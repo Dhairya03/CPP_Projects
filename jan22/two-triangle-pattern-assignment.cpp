47e937569487748770563194bc04c8ba00c7d9fb
Dhairya Gupta <dhairya.gupta@intimetec.com>
2024-01-24 23:59:47 +0530

    Pattern assignment commit

--- jan22/two-triangle-pattern-assignment.cpp
+++ jan22/two-triangle-pattern-assignment.cpp
@@ -1,100 +1,156 @@
-#include<iostream>
-using namespace std;
-void print_pattern(int n){
-    if(n%2==0){
-        for(int i=0;i<n+1;i++){
-            for(int j=0;j<n+1;j++){
-                if(j<=i || j>=2*n-i){
-                    if(j%2==0)cout<<"1";
-                    else cout<<"0";
-                }else cout<<" ";
-            }
-
-            for(int j=n+1;j<2*n+1;j++){
-                if(j<=i || j>=2*n-i){
-                    if((j-i)%2==0)cout<<"1";
-                    else cout<<"0";
-                }else cout<<" ";
-            }
-            cout<<endl;
-
-        }
-        for(int i=n+1;i<2*n+1;i++){
-            for(int j=0;j<n+1;j++){
-                if(j<=2*n-i || j>=i){
-                    if(j%2==0)cout<<"1";
-                    else cout<<"0";
-                }else cout<<" ";
-            }
-
-            for(int j=n+1;j<2*n+1;j++){
-                if(j<=2*n-i || j>=i){
-                    if((j-i)%2==0)cout<<"1";
-                    else cout<<"0";
-                }else cout<<" ";
-            }
-            cout<<endl;
-        }
-    }
-    else{
-        for(int i=0;i<n+1;i++){
-            for(int j=0;j<n+1;j++){
-                if(j<=i || j>=2*n+1-i){
-                    if(j%2==0)cout<<"1";
-                    else cout<<"0";
-                }else cout<<" ";
-            }
-
-            for(int j=n+1;j<2*n+2;j++){
-                if(j<=i || j>=2*n+1-i){
-                    if((j-i)%2==0)cout<<"0";
-                    else cout<<"1";
-                }else cout<<" ";
-            }
-            cout<<endl;
-
-        }
-        for(int i=n+1;i<2*n+1;i++){
-            for(int j=0;j<n+1;j++){
-                if(j<=2*n-i || j>i){
-                    if(j%2==0)cout<<"1";
-                    else cout<<"0";
-                }else cout<<" ";
-            }
-
-            for(int j=n+1;j<2*n+2;j++){
-                if(j<=2*n-i || j>i){
-                    if((j-i)%2==0)cout<<"0";
-                    else cout<<"1";
-                }else cout<<" ";
-            }
-            cout<<endl;
-        }
-    }
-    
+#include <iostream>
+#include <limits>
+void printCommonPattern(int column)
+{
+    if (column % 2 == 0)
+        std::cout << "1";
+    else
+        std::cout << "0";
 }
-int main(){
-    int n=0;
-    cout<<"Enter a number"<<endl;
-    //cout<<n;
-    while(true){
-        cin>>n;
-        //cout<<n<<endl;
-        if (!std::cin)
+
+void printAnotherCommonPattern(int column)
+{
+    if ((column) % 2 == 0)
+        std::cout << "0";
+    else
+        std::cout << "1";
+}
+
+void printEvenPattern(int inputNumber)
+{
+    for (int row = 0; row < inputNumber + 1; row++)
+    {
+        for (int column = 0; column < inputNumber + 1; column++)
         {
+            if (column <= row || column >= 2 * inputNumber - row)
+            {
+                printCommonPattern(column);
+            }
+            else
+                std::cout << " ";
+        }
+
+        for (int column = inputNumber + 1; column < 2 * inputNumber + 1; column++)
+        {
+            if (column <= row || column >= 2 * inputNumber - row)
+            {
+                printCommonPattern(column - row);
+            }
+            else
+                std::cout << " ";
+        }
+        std::cout << std::endl;
+    }
+    for (int row = inputNumber + 1; row < 2 * inputNumber + 1; row++)
+    {
+        for (int column = 0; column < inputNumber + 1; column++)
+        {
+            if (column <= 2 * inputNumber - row || column >= row)
+            {
+                printCommonPattern(column);
+            }
+            else
+                std::cout << " ";
+        }
+
+        for (int column = inputNumber + 1; column < 2 * inputNumber + 1; column++)
+        {
+            if (column <= 2 * inputNumber - row || column >= row)
+            {
+                printCommonPattern(column - row);
+            }
+            else
+                std::cout << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+void printOddPattern(int inputNumber)
+{
+    for (int row = 0; row < inputNumber + 1; row++)
+    {
+        for (int column = 0; column < inputNumber + 1; column++)
+        {
+            if (column <= row || column >= 2 * inputNumber + 1 - row)
+            {
+                printCommonPattern(column);
+            }
+            else
+                std::cout << " ";
+        }
+
+        for (int column = inputNumber + 1; column < 2 * inputNumber + 2; column++)
+        {
+            if (column <= row || column >= 2 * inputNumber + 1 - row)
+            {
+                printAnotherCommonPattern(column - row);
+            }
+            else
+                std::cout << " ";
+        }
+        std::cout << std::endl;
+    }
+    for (int row = inputNumber + 1; row < 2 * inputNumber + 1; row++)
+    {
+        for (int column = 0; column < inputNumber + 1; column++)
+        {
+            if (column <= 2 * inputNumber - row || column > row)
+            {
+                printCommonPattern(column);
+            }
+            else
+                std::cout << " ";
+        }
+
+        for (int column = inputNumber + 1; column < 2 * inputNumber + 2; column++)
+        {
+            if (column <= 2 * inputNumber - row || column > row)
+            {
+                printAnotherCommonPattern(column - row);
+            }
+            else
+                std::cout << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+void printPattern(int inputNumber)
+{
+    if (inputNumber % 2 == 0)
+    {
+        printEvenPattern(inputNumber);
+    }
+    else
+    {
+        printOddPattern(inputNumber);
+    }
+}
+int main()
+{
+    int inputNumber = -1;
+    std::cout << "Enter an integer" << std::endl;
+    while (true)
+    {
+        std::cin >> inputNumber;
+        if (std::cin.fail())
+        {
+            std::cout << inputNumber;
             if (std::cin.eof())
             {
-                exit(0); 
+                exit(0);
             }
-            std::cin.clear(); 
-            std::cin.ignore(INT16_MAX, '\n'); 
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         }
-        if(n<0 || n>=INT16_MAX){
-            cout<<"Invalid input!!!.Please try again"<<endl;
-            //cout<<n<<endl;
+        if (inputNumber < 0 || inputNumber >= INT16_MAX)
+        {
+            std::cout << "Invalid input!!!.Please try again.Enter " << std::endl;
         }
-        else{
-            print_pattern(n);
+        else
+        {
+            printPattern(inputNumber);
             return 0;
         }
     }
