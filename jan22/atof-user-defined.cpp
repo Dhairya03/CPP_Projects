ea455eee6eba920eac0e8333977751211e61519a
Dhairya Gupta <dhairya.gupta@intimetec.com>
2024-01-24 22:23:40 +0530

    User defined atof refactored code

--- jan22/atof-user-defined.cpp
+++ jan22/atof-user-defined.cpp
@@ -1,65 +1,93 @@
-#include<iostream>
-#include<cmath>
-using namespace std;
-double atof(const char* str){
-    
-    double result = 0.0;
-    int sign = 1;
-    double decimal=0.1;
-    int exponent_sign = 1;
-    int exponent=0;
+#include <iostream>
+#include <cmath>
 
-    while(*str == ' '){
-        str++;
+int checkSign(double &floatValue, std::string &inputString, int &index)
+{
+    int sign=1;
+    if (inputString[index] == '-' || inputString[index] == '+')
+    {
+        if (inputString[index] == '-')
+            sign = -1;
+        else
+            sign = 1;
+        index++;
     }
-
-    if(*str == '-' || *str == '+'){
-        if(*str == '-') sign = -1;
-        else sign=1;
-        str++;
-    }
-
-    while(*str >='0' && *str<='9'){
-        result=result*10+(*str-'0');
-        str++;
-    }
-
-    if(*str=='.'){
-        str++;
-        while(*str >='0' && *str<='9'){
-            result+=decimal*(*str-'0');
-            decimal*=0.1;
-            str++;
-        }
-    }
-
-    if(*str=='e'){
-        str++;  
-    if(*str=='-'||*str=='+'){
-        if(*str=='-')exponent_sign=-1;
-        else exponent_sign=1;
-        str++;
-    }
-
-    while(*str>='0'&& *str<='9'){
-        exponent=exponent*10+(*str-'0');
-        str++;
-    }
-    }
-
-    result*=sign;
-
-    if(exponent!=0){
-        result=result*pow(10,exponent*exponent_sign);
-    }
-    return result;
+    return sign;
 }
 
-int main(){
-    string input;
-    cin>>input;
-    cout<<"Enter input string"<<endl;
-    cout<<"Input String:"<<input<<endl;
-    cout<<"Result: "<<atof(&input[0])<<endl;
+void checkDecimal(double &floatValue, std::string &inputString, int &index)
+{
+    double decimal = 0.1;
+    if (inputString[index] == '.')
+    {
+        index++;
+        while (inputString[index] >= '0' && inputString[index] <= '9')
+        {
+            floatValue += decimal * (inputString[index] - '0');
+            decimal *= decimal;
+            index++;
+        }
+    }
+}
+
+void checkExponent(double &floatValue, std::string &inputString, int &index)
+{
+    int exponentSign = 1;
+    int exponent = 0;
+    if (inputString[index] == 'e')
+    {
+        index++;
+        exponentSign=checkSign(floatValue,inputString,index);
+
+        while (inputString[index] >= '0' && inputString[index] <= '9')
+        {
+            exponent = exponent * 10 + (inputString[index] - '0');
+            index++;
+        }
+    }
+    if (exponent != 0)
+    {
+        floatValue = floatValue * std::pow(10, exponent * exponentSign);
+    }
+}
+
+double atof(std::string &inputString)
+{
+    double floatValue = 0.0;
+    int sign = 1;
+    int index = 0;
+
+    while (inputString[index] == ' ')
+    {
+        index++;
+    }
+
+    sign=checkSign(floatValue,inputString,index);
+
+    while (inputString[index] >= '0' && inputString[index] <= '9')
+    {
+        floatValue = floatValue * 10 + (inputString[index] - '0');
+        index++;
+    }
+
+    checkDecimal(floatValue, inputString, index);
+
+    checkExponent(floatValue, inputString, index);
+
+    floatValue = floatValue * sign;
+
+    if (floatValue == -0)
+        return 0;
+
+    return floatValue;
+}
+
+int main()
+{
+    std::string inputString;
+    std::cout << "Enter input string" << std::endl;
+    std::getline(std::cin, inputString);
+    std::cout << "Input String:" << inputString << std::endl;
+    std::cout << "Float Value: " << atof(inputString) << std::endl;
     return 0;
 }
