9f413f681ddda4e6b592c94eca83211a30781b89
Dhairya Gupta <dhairya.gupta@intimetec.com>
2024-01-29 17:32:17 +0530

    January 29 library linking practice

---
+++ libraryDemo/add.cpp
@@ -0,0 +1,6 @@
+#include"demo.h"
+
+int add(int num1,int num2){
+    print("Addition:");
+    return num1+num2;
+}
---
+++ libraryDemo/main.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+#include"demo.h"
+
+int main(){
+    std::cout<<add(5,8)<<std::endl;
+    std::cout<<sub(8,6)<<std::endl;
+}
