3bd94b6b345bc2bdff8f7875fc2dcf37327c29f1
Dhairya Gupta <dhairya.gupta@intimetec.com>
2024-01-29 17:40:55 +0530

    January 29 dynamic linking practice

---
+++ calculatorLibrary/application.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "../include/header.h"
+
+
+int  main(){
+    std::cout<<"ADD"<<add(6,8)<<std::endl;
+    std::cout<<"SUB"<<sub(8,3)<<std::endl;
+    std::cout<<"MUL"<<multiply(2,4)<<std::endl;
+    std::cout<<"DIV"<<divide(4,2)<<std::endl;
+
+
+}
