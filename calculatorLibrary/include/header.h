c650a532a557095f7e309e6f1589c2c3974334fb
Dhairya Gupta <dhairya.gupta@intimetec.com>
2024-01-30 23:56:51 +0530

    Calculator using dynamic library

---
+++ calculatorLibrary/include/header.h
@@ -0,0 +1,7 @@
+double add(double num1,double num2);
+
+double subtract(double num1,double num2);
+
+double multiply(double num1,double num2);
+
+double divide(double num1,double num2);
---
+++ calculatorLibrary/source/subtraction.cpp
@@ -0,0 +1,3 @@
+double subtract(double num1,double num2){
+    return num1-num2;
+}
