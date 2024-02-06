460acb779703b6d0f80ba1d2fe47ca0bd176f5db
Dhairya Gupta <dhairya.gupta@intimetec.com>
2024-02-06 23:28:17 +0530

    resolved conflicts

---
+++ matrix/src/matrixMultiplication.cpp
@@ -0,0 +1,19 @@
+#include "matrixMultiplication.h"
+
+double **multiplyMatrices(double **matrixOne, double **matrixTwo, int size, double **outputMatrix)
+{
+    for (int row = 0; row < size; row++)
+    {
+        for (int column = 0; column < size; column++)
+        {
+            double value = 0.0;
+            for (int index = 0; index < size; index++)
+            {
+                value += *(*(matrixOne + row) + index) * *(*(matrixTwo + index) + column);
+            }
+            outputMatrix[row][column] = value;
+        }
+    }
+
+    return outputMatrix;
+}
