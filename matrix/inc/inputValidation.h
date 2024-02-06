1f5bdbcae386f3ef2de22816ad01bf7bf1c08158
Dhairya Gupta <dhairya.gupta@intimetec.com>
2024-02-06 23:28:17 +0530

    Matrix addition and multiplication using pointers

---
+++ matrix/inc/inputValidation.h
@@ -0,0 +1,12 @@
+#ifndef _INPUTVALIDATION_H
+#define _INPUTVALIDATION_H
+
+bool isValidIntegerInput(int inputNumber);
+
+bool isValidDoubleInput(double inputNumber);
+
+bool isValidSize(int size);
+
+#endif
+
+
---
+++ matrix/inc/matrixMultiplication.h
@@ -0,0 +1,6 @@
+#ifndef _MULTIPLICATION_H
+#define _MULTIPLICATION_H
+
+double **multiplyMatrices(double **matrixOne, double **matrixTwo, int size, double **outputMatrix);
+
+#endif
