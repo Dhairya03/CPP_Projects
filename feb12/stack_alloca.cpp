5b6e5916eaffec8454679f1194110e2f8cb7509f
Dhairya Gupta <dhairya.gupta@intimetec.com>
2024-02-16 16:02:54 +0530

    Modified stack allcoation

--- feb12/stack_alloca.cpp
+++ feb12/stack_alloca.cpp
@@ -8,6 +8,17 @@ int main(){
     dataptr=(int*)alloca(40);
     int *dataptr2=NULL;
     dataptr2=(int*)alloca(sizeof(int)*5);
+
+    int*dataptr3=NULL;
+        dataptr3=(int*)malloc(sizeof(int)*5);
+
+int *dataptr4=NULL;
+    dataptr4=(int*)alloca(sizeof(int)*50);
+
+    int*dataptr5=NULL;
+        dataptr5=(int*)malloc(sizeof(int)*50);
+
+
     for(int i=0;i<5;i++){
         std::cout<<&dataptr2[i]<<std::endl;
     }
@@ -15,6 +26,10 @@ int main(){
     std::cout<<&dataptr2<<std::endl;
     std::cout<<dataptr<<std::endl;
     std::cout<<&dataptr<<std::endl;
+    std::cout<<&dataptr3<<std::endl;
+    std::cout<<dataptr3<<std::endl;
+    free(dataptr3);
+    free(dataptr5);
    // free(dataptr);
     std::cin.get();
 }
