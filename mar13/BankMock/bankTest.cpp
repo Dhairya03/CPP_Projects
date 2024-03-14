3ffe65fed017b4568d260252a7e66040c4f65afd
Dhairya Gupta <dhairya.gupta@intimetec.com>
2024-03-14 08:55:43 +0530

    onCall Function refactor

--- mar13/BankMock/bankTest.cpp
+++ mar13/BankMock/bankTest.cpp
@@ -1,22 +1,33 @@
-#include<gtest/gtest.h>
-#include<iostream>
-#include"mockBankServer.h"
-#include"atmMachine.cpp"
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include <iostream>
+#include "mockBankServer.h"
+#include "atmMachine.cpp"
 
+using ::testing::Return;
 
-TEST(AtmMachine,canWithdarw){
-    const int accNumber=1234;
-    const int withdraw_value=1000;
+TEST(Atm_Machine, onCallWithOneFunctionSpecified)
+{
+    const int accNumber = 1234;
+    const int withdraw_value = 1000;
 
     MockBankServer bankServer;
 
-    
+    ON_CALL(bankServer, Connect).WillByDefault(Return());
 
+    ON_CALL(bankServer, GetBalance(accNumber)).WillByDefault(Return(2000));
 
+    ON_CALL(bankServer, Withdraw).WillByDefault(Return());
+
+    ON_CALL(bankServer, Disconnect).WillByDefault(Return());
+
+    ON_CALL(bankServer,Credit).WillByDefault(Return());
 
     AtmMachine atm_machine(&bankServer);
 
-    bool withdraw_result=atm_machine.Withdraw(1234,1000);
+    bool withdraw_result1 = atm_machine.Withdraw(accNumber, withdraw_value);
+    bool withdraw_result2 = atm_machine.Withdraw(accNumber + 1, withdraw_value);
 
-    EXPECT_FALSE(withdraw_result);
+    EXPECT_TRUE(withdraw_result1);
+    EXPECT_FALSE(withdraw_result2);
 }
