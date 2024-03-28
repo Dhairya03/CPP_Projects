560384ef2194cd6cb6363855a2e992971b229528
Dhairya Gupta <dhairya.gupta@intimetec.com>
2024-03-28 17:31:52 +0530

    March 28 daily code

---
+++ mar28/thread.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <thread>
+
+static bool isFinished = false;
+
+void DoWork()
+{
+    using namespace std::literals::chrono_literals;
+    while (!isFinished)
+    {
+        std::cout << "Working..." << std::endl;
+        std::this_thread::sleep_for(1s);
+    }
+}
+
+int main()
+{
+    std::thread worker(DoWork);
+    std::cin.get();
+    isFinished = true;
+    worker.join();
+    std::cout << "Finished" << std::endl;
+    std::cin.get();
+}
